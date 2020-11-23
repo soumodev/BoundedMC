4654be846a014285c8746b8cf559da6c8f0252cb
Diganta Mukhopadhyay <diganta.ghs@gmail.com>
2020-11-23 21:44:39 +0530

    Bugfixes

--- Invariant_Liveness.py
+++ Invariant_Liveness.py
@@ -1,5 +1,5 @@
 from z3 import *
-#the Kripke encoding
+the Kripke encoding
 def init(s):
     return(And(s[0],Not(s[1])))
 
@@ -47,7 +47,7 @@ def Invariant_Check(n,k,init,trans,p):
         print(s.model())
         return "Invariant doesn't hold and there is a counterexample"
 
-# BMC for Fp
+ BMC for Fp
 def Invariant_Check_Fp(n_bits, threshold, init, trans, p):
     """
     Check if there are lassoing cex to Fp of length less than threshold
@@ -63,10 +63,13 @@ def Invariant_Check_Fp(n_bits, threshold, init, trans, p):
     s.add(And(Not(p(st[0])), Not(p(st[1]))))
 
     for k in range(1, threshold+1):
+        # DEBUG
+        print("Looking for cex of size %d"%k)
+
         # Set backtrack point before lasso constriant
         s.push()
         # Add lasso expr for k
-        s.add(Or(And( Not(Xor(p, q)) for p, q in zip sti, st[k]) for sti in st[:-1]))
+        s.add(Or([ And([ Not(Xor(p, q)) for p, q in zip (sti, st[k]) ]) for sti in st[:-1] ]))
         # check if cex
         if s.check() == sat:
             print("Found CEX of length %d:"%k)
@@ -81,4 +84,15 @@ def Invariant_Check_Fp(n_bits, threshold, init, trans, p):
         # Add path and cex conditions
         s.add(trans(st[k], st[k+1]))
         s.add(Not(p(st[k+1])))
-        return "Invariant doesn't hold and there is a counterexample"
+
+# DEBUG
+from parse_to_z3 import *
+
+n_bits = 2
+init = "((!v0) . (!v1))"
+trans = "(((!u1) . v1) + ((u0 . (v0 . (u1 . (!v1)))) + (((!u0) . (v0 . (u1 . (!v1)))) \
+        + (u0 . ((!v0) . ((!u1) . (!v1)))))))"
+pred = "(v0 . v1)"
+
+Invariant_Check_Fp(n_bits, 5, parse_pred_z3_gen(init, n_bits), parse_trans_z3_gen(trans, n_bits),
+        parse_pred_z3_gen(pred, n_bits))
