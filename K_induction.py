c22788e9eeee097c698b0126712a513a80071e04
soumodev <44368973+soumodev@users.noreply.github.com>
2020-11-24 03:04:40 +0530

    Updated K_ind

--- K_induction.py
+++ K_induction.py
@@ -1,24 +1,32 @@
 from reocc_diam import *
 
 def K_induction(n,init,trans,p):
-    j=k=get_reocc_diam(n,init,trans)
-    S_N_prime = [[Bool("x_%s_%s" % (j-k,i+1)) for i in range(n)]]
+    S_N_prime = [[Bool("x_%s_%s" % (0,i+1)) for i in range(n)]]
     s=Solver()
-    s.add(init(S_N_prime[0]))
+    s1=Solver()
     s.push()
-    s.add(Not(p(S_N_prime[0])))
-    if(s.check() == unsat):
-        while(k>0):
+    s1.add(init(S_N_prime[0]))
+    s1.push()
+    s1.add(Not(p(S_N_prime[0])))
+    if(s1.check() == unsat):
+        k=1
+        while(True):
             s.pop()
-            print("Checking for CEX after %d transitions"%(j-k+1))
-            S_N_prime.append([Bool("x_%s_%s" %(j-k+1,i+1)) for i in range(n)])
-            s.add(trans(S_N_prime[j-k],S_N_prime[j-k+1]))
+            print("Checking for CEX after %d transitions"%(k))
+            S_N_prime.append([Bool("x_%s_%s" % (k,i+1)) for i in range(n)])
+            s.add(And(p(S_N_prime[k-1]),trans(S_N_prime[k-1],S_N_prime[k])))
             s.push()
-            s.add(Not(p(S_N_prime[j-k+1])))
-            if(s.check() == sat):
-                print(s.model())
-                return "Invariant doesn't hold and there is a counterexample"
-            k-=1
+            print(s)
+            s.add(Not(p(S_N_prime[k])))
+            if(s.check()==unsat):
+                return "Verified"
+            s1.pop()
+            s1.add(trans(S_N_prime[k-1],S_N_prime[k]))
+            s1.push()
+            s1.add(Not(p(S_N_prime[k])))
+            if(s1.check()==sat):
+                return "CounterExample"
+            k+=1
         return "The invariant holds!"
     else:
         print(s.model())
