ebe3b0ff4f483fa763ecb4b268b0a8d5ab62b9c8
soumodev <44368973+soumodev@users.noreply.github.com>
2020-11-23 21:30:35 +0530

    Update Invariant_Liveness.py
    updated the Invariant_Check function

--- Invariant_Liveness.py
+++ Invariant_Liveness.py
@@ -1,18 +1,6 @@
 from z3 import *
+
 #the Kripke encoding
-def init(s):
-    return(And(s[0],Not(s[1])))
-
-def p(s):
-    return(s[0]==False)
-
-def trans(n,s):
-    s_prime = [Bool("y_%s" % (i+1)) for i in range(n)] 
-    s1 = Solver()
-    s1.add(Xor(s[0],s_prime[0]))
-    return s1,s_prime
-
-#the code for checking invariant
 def init(s):
     return(And(s[0],Not(s[1])))
 
@@ -22,20 +10,19 @@ def p(s):
 def trans(s,s_p):
     return(s[0]==s_p[0])
 
+#the code for checking invariant
 def Invariant_Check(n,k,init,trans,p):
-    S_0 = [Bool("x_%s" % (i+1)) for i in range(n)]
+    S_N_prime = [[Bool("x_%s" % (i+1)) for i in range(n)],[Bool("x_%s_%s" %(k,i+1)) for i in range(n)]]
     s=Solver()
-    s.add(init(S_0))
-    s.add(Not(p(S_0)))
-    s.push()
+    s.add(And(init(S_N_prime[0]),trans(S_N_prime[0],S_N_prime[1])))
+    s.add(And(Not(p(S_N_prime[0])),Not(p(S_N_prime[1]))))
     if(s.check() == unsat):
-        s.pop()
-        S_N=S_0
-        S_N_prime = [Int("x_%s_%s" %(i+1,k)) for i in range(n)]
+        i=k
         while(k!=0):
-            s.add(trans(S_N,S_N_prime))
-            s.add(Not(p(S_N_prime)))
+            S_N_prime.append([Bool("x_%s_%s" %(k-1,i+1)) for i in range(n)])
             s.push()
+            s.add(trans(S_N_prime[i-k],S_N_prime[i-k+1]))
+            s.add(Not(p(S_N_prime[i-k+1])))
             S_N=S_N_prime
             if(s.check == sat):
                 print(s.model())
